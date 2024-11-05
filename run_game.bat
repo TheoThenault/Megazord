3d5403606787b7a8934b4b047a0611b1df5da093
TheoThenault <theo.thenault@gmail.com>
2024-11-05 16:41:42 +0100

    Modif

--- run_game.bat
+++ run_game.bat
@@ -1,1 +1,1 @@
-halite.exe --replay-directory replays/ -vvv --width 64 --height 64 "./HaliteAI/Debug/HaliteAI.exe" "MyBot.exe" "MyBot.exe" "MyBot.exe"
+halite.exe --replay-directory replays/ -vvv --width 64 --height 64 ".\HaliteAI\Debug\HaliteAI.exe" "MyBot.exe" "MyBot.exe" "MyBot.exe"
